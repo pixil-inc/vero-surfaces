cf6f55213b741ba2373a726764c312dc10cc99a6
Shavar Sean Cox <shavarsean@gmail.com>
2026-02-09 23:21:25 -0500

    bootstrap next.js

---
+++ components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Header: React.FC = () => {
+  return (
+    <header className="flex items-center justify-between p-4 bg-gray-800 text-white">
+      <h1 className="text-xl font-bold">Vero Surfaces</h1>
+    </header>
+  );
+};
+
+export default Header;
